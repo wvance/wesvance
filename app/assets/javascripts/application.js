eeb9e51130709f61e5cdacda751a23f81d751fa2
Wesley Vance <wesley.vance@mac.com>
2015-07-17 19:00:38 -0400

    FIXED ANCHOR SMOOTH SCROLLING

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,20 +31,22 @@ function setGeoCookie(position) {
 }
 
 // FUNCTION FOR SMOOTH SCROLLING
-// $(document).ready(function(){
-//   $('a[href*=#]:not([href=#])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html,body').animate({
-//           scrollTop: target.offset().top
-//         }, 1250);
-//         return false;
-//       }
-//     }
-//   });
-// });
+$(function() {
+  // CHECK IF LINK
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        console.log("Success");
+        return false;
+      }
+    }
+  });
+});
 
 // AUTOGROW TEXT AREAS
 $(document).ready(function(){
