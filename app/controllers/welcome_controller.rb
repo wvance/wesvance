b117c709c8e9bdbd5252598706aefee205a60ae1
Wesley Vance <wesley.vance@mac.com>
2015-07-24 00:55:24 -0400

    Fixed Date DESC

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@posts = Post.all.order('posts.title ASC').page(params[:page]).per(6)
+  	@posts = Post.all.order('posts.date DESC').page(params[:page]).per(6)
   	@post = Post.new(:is_active => true)
 
 
