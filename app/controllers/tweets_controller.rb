3925c96ac74996bf4c925a598048eb9f07a02eea
Wesley Vance <wesley.vance@mac.com>
2015-08-08 12:13:39 -0400

    Attempt fix for Twitter

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -33,9 +33,9 @@ class TweetsController < ApplicationController
     @tweet.tweet_id = timeline.id
     @tweet.body = timeline.text
     @tweet.user = timeline.user.screen_name
-    @tweet.image = timeline.media[0]["#media_url"]
+    @tweet.image = timeline.media[0]["media_url"]
     @tweet.location = timeline.place.full_name
-    @tweet.url = timeline.media[0]["#url"]
+    @tweet.url = timeline.media[0].url
 
     respond_to do |format|
       if @tweet.save
